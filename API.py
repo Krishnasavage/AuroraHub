98b170996cfb42c1e04aba1e2d9199e56776e903
KinaSoYoung <79552332+Krishnasavage@users.noreply.github.com>
2026-06-05 15:55:47 +0800

    Update API.py

--- API.py
+++ API.py
@@ -68,4 +68,4 @@ def redeem():
     return jsonify({'success': True if row[1] == data['hwid'] else False})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0', port=5000)
